83197dddf78dad1388b95da1891fb3f521f4a674
zkalazny <128413999+zkalazny@users.noreply.github.com>
2023-04-04 18:35:37 +0200

    Add files via upload

---
+++ php_04_szablonowanie/calc.php
@@ -0,0 +1,99 @@
+<?php
+require_once dirname(__FILE__).'/../config.php';
+require_once _ROOT_PATH.'/lib/smarty/Smarty.class.php';
+
+function getParams(&$form){
+	$form['x'] = isset($_REQUEST['x']) ? $_REQUEST['x'] : null;
+	$form['y'] = isset($_REQUEST['y']) ? $_REQUEST['y'] : null;
+	$form['op'] = isset($_REQUEST['op']) ? $_REQUEST['op'] : null;	
+}
+function validate(&$form,&$infos,&$msgs,&$hide_intro){
+
+	
+	if ( ! (isset($form['x']) && isset($form['y']) && isset($form['op']) ))	return false;	
+	
+	$hide_intro = true;
+
+	$infos [] = 'Przekazano parametry.';
+
+	
+	if ( $form['x'] == "") $msgs [] = 'Nie podano liczby 1';
+	if ( $form['y'] == "") $msgs [] = 'Nie podano liczby 2';
+	
+	
+	if ( count($msgs)==0 ) {
+		
+		if (! is_numeric( $form['x'] )) $msgs [] = 'Pierwsza wartość nie jest liczbą';
+		if (! is_numeric( $form['y'] )) $msgs [] = 'Druga wartość nie jest liczbą';
+	}
+	
+	if (count($msgs)>0) return false;
+	else return true;
+}
+function process(&$form,&$infos,&$msgs,&$result){
+	$infos [] = 'Parametry poprawne. Wykonuję obliczenia.';
+	
+	$form['x'] = floatval($form['x']);
+	$form['y'] = floatval($form['y']);
+	$month = 0;
+	$loan = 0;
+	$percent = 0;
+	
+	switch ($form['op']) {
+		case '4' :
+			$month = ($form['y'])*12;
+			$loan = ($form['x'])/$month;
+			$percent = ((4*$loan)/100);
+			$result = $loan + $percent;
+			$form['op_name'] = '4%';
+			break;
+		case '6' :
+			$month = ($form['y'])*12;
+			$loan = ($form['x'])/$month;
+			$percent = ((6*$loan)/100);
+			$result = $loan + $percent;
+			$form['op_name'] = '6%';
+			break;
+		case '8' :
+			$month = ($form['y'])*12;
+			$loan = ($form['x'])/$month;
+			$percent = ((8*$loan)/100);
+			$result = $loan + $percent;
+			$form['op_name'] = '8%';
+			break;
+		default :
+			$month = ($form['y'])*12;
+			$loan = ($form['x'])/$month;
+			$percent = ((2*$loan)/100);
+			$result = $loan + $percent;
+			$form['op_name'] = '2%';
+			break;
+	}
+}
+$form = null;
+$infos = array();
+$messages = array();
+$result = null;
+
+	
+getParams($form);
+if ( validate($form,$infos,$messages,$hide_intro) ){
+	process($form,$infos,$messages,$result);
+}
+
+$smarty = new Smarty();
+
+$smarty->assign('app_url',_APP_URL);
+$smarty->assign('root_path',_ROOT_PATH);
+$smarty->assign('page_title','Kalkulator kredytowy');
+$smarty->assign('page_description','Profesjonalne szablonowanie oparte na bibliotece Smarty');
+$smarty->assign('page_header','Szablon Smarty');
+
+
+$smarty->assign('form',$form);
+$smarty->assign('result',$result);
+$smarty->assign('messages',$messages);
+$smarty->assign('infos',$infos);
+
+
+$smarty->display(_ROOT_PATH.'/app/calc.html');
---
+++ php_04_szablonowanie/config.php
@@ -0,0 +1,8 @@
+<?php
+define('_SERVER_NAME', 'localhost:80');
+define('_SERVER_URL', 'http://'._SERVER_NAME);
+define('_APP_ROOT', '/php_04_szablonowanie');
+define('_APP_URL', _SERVER_URL._APP_ROOT);
+define("_ROOT_PATH", dirname(__FILE__));
+
+?>
